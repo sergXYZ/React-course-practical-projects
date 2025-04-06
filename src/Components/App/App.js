018d9515e05a0a3ccf83172a5f53fba0fb313c46
sergXYZ <serhiis1993@gmail.com>
2025-04-07 00:25:15 +0200

    upgrade

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styles from "./App.module.css";
 import SearchResults from "../SearchResult/SearchResult";
+import Playlist from "../Playlist/Playlist";
 
 function App () {
   // eslint-disable-next-line
@@ -17,7 +18,41 @@ function App () {
       album: "example track album 2",
       id: 2
     }]
-  )
+  );
+  const[playlistName, setPlaylistName] = useState('Random playlist name');
+const[playlistTracks, setPlaylistTtracks] = useState([
+  {
+    name: "example playlist name 1",
+    artist: "example playlist artist 1",
+    album: "example playlist album 1",
+    id: 1
+  },
+  {
+    name: "example playlist name 2",
+    artist: "example playlist artist 2",
+    album: "example playlist album 2",
+    id: 2
+  },
+  {
+    name: "example playlist name 3",
+    artist: "example playlist artist 3",
+    album: "example playlist album 3",
+    id: 3
+  }
+  
+
+]);
+
+function addTrack(track) {
+const existingTrack = playlistTracks.find((t) => t.id === track.id);
+const newTrack = playlistTracks.concat(track);
+if(existingTrack) {
+  console.log(`Track ${existingTrack} is already in a playlist!`)
+} else {
+  setPlaylistTtracks(newTrack);
+}
+};
+
     return (
         <div>
         <h1>
@@ -30,6 +65,7 @@ function App () {
             {/* <!-- Add a SearchResults component --> */}
             <SearchResults userSearchResult={searchResults}/>
             {/* <!-- Add a Playlist component --> */}
+            <Playlist playlistName={playlistName} playlistTracks={playlistTracks}/>
           </div>
         </div>
       </div>
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from './Playlist.module.css';
+import Tracklist from "../TrackList/TrackList";
 
-function Playlist() {
+function Playlist(props) {
   return (
     <div className={styles.Playlist}>
       <input defaultValue={"New Playlist"} />
       {/* <!-- Add a TrackList component --> */}
+      <Tracklist userSearchResults={props.playlistTracks}/>
       <button className={styles["Playlist-save"]}>
         SAVE TO SPOTIFY
       </button>
