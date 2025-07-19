e92076ed571748b36efaf71bfadcede55876c6f5
sergXYZ <serhiis1993@gmail.com>
2025-07-19 23:34:50 +0200

    first commit

---
+++ sidebar/src/App.jsx
@@ -0,0 +1,14 @@
+import Home from './Home'
+import SIdebar from './SIdebar'
+import Modal from './Modal'
+
+const App = () => {
+  return (
+    <>
+      <Home />
+      <Modal />
+      <SIdebar />
+    </>
+  )
+}
+export default App
